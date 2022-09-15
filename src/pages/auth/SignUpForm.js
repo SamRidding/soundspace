63f8b80fee5095f1ae07cd0f9d22bbe6a56cd901
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-15 21:00:54 +0000

    Add usestate hook and value props to sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/SignUpForm.module.css"
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
   return (
     <div className={styles.center}>
       <form>
@@ -11,18 +18,21 @@ const SignUpForm = () => {
             type="text"
             placeholder="Enter Username"
             name="username"
+            value={username}
             required
           ></input>
           <input
             type="text"
             placeholder="Enter Password"
             name="password1"
+            value={password1}
             required
           ></input>
           <input
             type="text"
             placeholder="Confirm Password"
             name="password2"
+            value={password2}
             required
           ></input>
           <div>
