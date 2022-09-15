308295e5c9b1efc31de0d5098fc381a38c002859
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-15 16:17:10 +0000

    Add routes for nav links in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home page</h1>} />
+        <Route exact path="/login" render={() => <h1>Log in</h1>} />
+        <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+      </Switch>
     </div>
   );
 }
