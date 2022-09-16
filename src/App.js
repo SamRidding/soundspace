fa04e89b89a82147cbd2118473185ef2a1f05cc9
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-16 14:21:00 +0000

    Create log in form with stylesheet and import to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults'
 import SignUpForm from "./pages/auth/SignUpForm";
+import LogInForm from "./pages/auth/LogInForm";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <NavBar />
       <Switch>
         <Route exact path="/" render={() => <h1>Home page</h1>} />
-        <Route exact path="/login" render={() => <h1>Log in</h1>} />
+        <Route exact path="/login" render={() => <LogInForm />} />
         <Route exact path="/signup" render={() => <SignUpForm />} />
       </Switch>
     </div>
