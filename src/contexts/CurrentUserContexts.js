64fe00da7cbf5ce45f1add6b90d982f0db024202
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-16 17:30:43 +0000

    Add axios request interceptor to current user context

--- src/contexts/CurrentUserContexts.js
+++ src/contexts/CurrentUserContexts.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { axiosRes } from "../api/axiosDefaults";
+import { axiosRes, axiosReq } from "../api/axiosDefaults";
 import { useHistory } from "react-router-dom";
 
 export const CurrentUserContext = createContext();
@@ -27,26 +27,46 @@ export const CurrentUserProvider = ({ children }) => {
   }, []);
 
   useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history.push("/login");
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+
     axios.interceptors.response.use(
       (response) => response,
       async (err) => {
-        if (err.response?.status === 401){
-          try{
-            await axios.post('/dj-rest-auth/token/refresh/')
-          } catch(err){
-            setCurrentUser(prevCurrentUser => {
-              if (prevCurrentUser){
-                history.push('/login')
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/login");
               }
-              return null
-            })
+              return null;
+            });
           }
-          return axios(err.config)
+          return axios(err.config);
         }
-        return Promise.reject(err)
+        return Promise.reject(err);
       }
-    )
-  })
+    );
+  }, [history]);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
