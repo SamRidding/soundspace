f68f9f4c97d491d808938d258061a532029cc519
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-16 14:25:01 +0000

    Add basic form to log in form page

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "../../styles/LogInForm.module.css";
 
 const LogInForm = () => {
   return (
-    <div>LogInForm</div>
-  )
-}
+    <div className={styles.center}>
+      <form>
+        <div className={styles.container}>
+          <h1>LOG IN</h1>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            required
+          ></input>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password1"
+            required
+          ></input>
+          <div>
+            <button type="submit">Log In</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default LogInForm
-
+export default LogInForm;
