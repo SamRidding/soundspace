d872b9f784e2f45aafce30db5a52c7937accfa7d
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-17 11:35:08 +0000

    Add user pic file and stylesheet

---
+++ src/components/UserPic.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles/UserPic.module.css";
+
+const UserPic = ({ src, height = 45, text }) => {
+  return (
+    <span>
+      <img
+        className={styles.UserPic}
+        src={src}
+        height={height}
+        width={height}
+        alt="avatar"
+      />
+      {text}
+    </span>
+  );
+};
+
+export default UserPic;
