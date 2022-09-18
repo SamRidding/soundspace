199e39a98e79fd1e6af0b3b911b6973853de26d5
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-18 12:44:45 +0000

    Create track upload form with basic styling

---
+++ src/pages/tracks/TrackUploadForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "../../styles/TrackUploadForm.module.css"
+
+const TrackUploadForm = () => {
+
+  return (
+    <div className={styles.center}>
+      <form>
+        <div className={styles.container}>
+          <h1>Upload Track</h1>
+          <input
+            type="text"
+            placeholder="Title"
+            name="title"
+          ></input>
+          <input
+            type="url"
+            placeholder="Audio Link"
+            name="audio"
+          ></input>
+          <input
+            type="file"
+            placeholder="Image"
+            name="image"
+          ></input>
+          <input
+            type="text"
+            placeholder="Track Info"
+            name="content"
+          ></input>
+          <select name="status" id="status">
+            <option value="DRAFT">DRAFT</option>
+            <option value="PUBLISHED">PUBLISHED</option>
+          </select>
+          <div>
+            <button type="submit">Post Track</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TrackUploadForm;
