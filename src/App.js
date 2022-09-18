22c9d6e488f5724a68eefe08cec5cbdb4dc615e1
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-18 12:45:20 +0000

    Add route path for track uploads

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import LogInForm from "./pages/auth/LogInForm";
+import TrackUploadForm from "./pages/tracks/TrackUploadForm";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" render={() => <h1>Home page</h1>} />
         <Route exact path="/login" render={() => <LogInForm />} />
         <Route exact path="/signup" render={() => <SignUpForm />} />
+        <Route exact path="/tracks/upload" render={() => <TrackUploadForm />} />
       </Switch>
     </div>
   );
