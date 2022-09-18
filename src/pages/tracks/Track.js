d2f6140896e74ed37538f6d559aede4639ef30f8
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-18 17:54:02 +0000

    Display user profile image on track page

--- src/pages/tracks/Track.js
+++ src/pages/tracks/Track.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "../../styles/Track.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContexts";
+import { Link } from "react-router-dom";
+import UserPic from "../../components/UserPic"
 
 const Track = (props) => {
   const {
@@ -16,12 +18,19 @@ const Track = (props) => {
     posted_at,
     likes_count,
     comments_count,
+    profile_img,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
-  return <div>Track</div>;
+  return(
+    <div>
+        <Link to={`/profiles/${profile_id}`}>
+            <UserPic src={profile_img} />
+        </Link>
+    </div>
+  );
 };
 
 export default Track;
