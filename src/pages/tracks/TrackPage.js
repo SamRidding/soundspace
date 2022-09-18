61b30b4a97d0101103aac89248219604244ea67b
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-18 16:40:22 +0000

    Import use params and use state to track page

--- src/pages/tracks/TrackPage.js
+++ src/pages/tracks/TrackPage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 import styles from '../../styles/TrackPage.module.css'
 
 const TrackPage = () => {
+    const { id } = useParams();
+    const [track, setTrack] = useState();
+
   return (
     <div>TrackPage</div>
   )
