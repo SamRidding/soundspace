c04a0ced7709b00fe80acc856deb950cbccf6103
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-18 17:43:31 +0000

    Destructure props from track results

--- src/pages/tracks/Track.js
+++ src/pages/tracks/Track.js
@@ -1,7 +1,24 @@
 import React from "react";
 import styles from "../../styles/Track.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContexts";
 
-const Track = () => {
+const Track = (props) => {
+    const {
+        id,
+        owner,
+        title,
+        audio,
+        image,
+        content,
+        status,
+        edited_at,
+        is_owner,
+        profile_id,
+        posted_at,
+        likes_count,
+        comments_count,
+      } = props;
+
   return <div>Track</div>;
 };
 
