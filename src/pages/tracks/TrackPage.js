888acfc80994815bc674dd7f260620ad159a217b
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-18 16:51:41 +0000

    Add use effect hook to return track data

--- src/pages/tracks/TrackPage.js
+++ src/pages/tracks/TrackPage.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import styles from '../../styles/TrackPage.module.css'
+import { axiosReq } from "../../api/axiosDefaults";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "../../styles/TrackPage.module.css";
 
 const TrackPage = () => {
-    const { id } = useParams();
-    const [track, setTrack] = useState({ results: [] });
+  const { id } = useParams();
+  const [track, setTrack] = useState({ results: [] });
 
-  return (
-    <div>TrackPage</div>
-  )
-}
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: track }] = await Promise.all([
+          axiosReq.get(`/tracks/${id}`),
+        ]);
+        setTrack({ results: [track] });
+        console.log(track);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-export default TrackPage
+    handleMount();
+  }, [id]);
+
+  return <div>TrackPage</div>;
+};
+
+export default TrackPage;
