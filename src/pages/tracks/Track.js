e1269b19597d766e8c433eea1fc62a0e7152e41a
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-18 17:46:58 +0000

    Import use current user hook and check if current user is owner

--- src/pages/tracks/Track.js
+++ src/pages/tracks/Track.js
@@ -3,21 +3,23 @@ import styles from "../../styles/Track.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContexts";
 
 const Track = (props) => {
-    const {
-        id,
-        owner,
-        title,
-        audio,
-        image,
-        content,
-        status,
-        edited_at,
-        is_owner,
-        profile_id,
-        posted_at,
-        likes_count,
-        comments_count,
-      } = props;
+  const {
+    id,
+    owner,
+    title,
+    audio,
+    image,
+    content,
+    status,
+    edited_at,
+    profile_id,
+    posted_at,
+    likes_count,
+    comments_count,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return <div>Track</div>;
 };
