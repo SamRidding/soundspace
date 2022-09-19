f13b3a200e2f8e47dabac2ff615a8371e9aacf22
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-19 20:44:44 +0000

    Add use effect to filter tracks from api

--- src/pages/tracks/TracksPage.js
+++ src/pages/tracks/TracksPage.js
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function TracksPage({ filter = "" }) {
   const [tracks, setTracks] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false)
-  
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const { data } = await axiosReq.get(`/tracks/?${filter}`);
+        setTracks(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchTracks();
+  }, [filter]);
+
   return <div>Track Page</div>;
 }
 
