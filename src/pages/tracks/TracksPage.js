ca2f4de26304d718c05400609f22a24a597f65c7
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-20 14:46:06 +0000

    Add search bar to home page

--- src/pages/tracks/TracksPage.js
+++ src/pages/tracks/TracksPage.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import Track from "./Track";
 import Loading from "../../components/Loading";
+import styles from "../../styles/TracksPage.module.css"
 
 function TracksPage({ filter = "" }) {
   const [tracks, setTracks] = useState({ results: [] });
-  const [hasLoaded, setHasLoaded] = useState(false)
+  const [hasLoaded, setHasLoaded] = useState(false);
 
   useEffect(() => {
     const fetchTracks = async () => {
@@ -22,27 +23,37 @@ function TracksPage({ filter = "" }) {
     fetchTracks();
   }, [filter]);
 
-  return(
+  return (
     <div>
+      <div className={styles.wrap}>
+        <div className={styles.search}>
+          <input
+            type="text"
+            className={styles.searchTerm}
+            placeholder="Search tracks"
+          ></input>
+          <button type="submit" class={styles.searchButton}>
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
       {hasLoaded ? (
-          <>
-            {tracks.results.length ? (
-              tracks.results.map((track) => (
-                <Track key={track.id} {...track} setTracks={setTracks} />
-              ))
-            ) : (
-              <div>
-                No results.
-              </div>
-            )}
-          </>
-        ) : (
-          <div>
-            <Loading />
-          </div>
-        )}
+        <>
+          {tracks.results.length ? (
+            tracks.results.map((track) => (
+              <Track key={track.id} {...track} setTracks={setTracks} />
+            ))
+          ) : (
+            <div>No results.</div>
+          )}
+        </>
+      ) : (
+        <div>
+          <Loading />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default TracksPage;
