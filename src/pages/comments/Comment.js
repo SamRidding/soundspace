2900b549c9f4ae1f6a2d37f3639bfa97c9b1e292
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-21 21:14:26 +0000

    Add handle delete function and add on click prop

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,13 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
 import UserPic from "../../components/UserPic";
 import { useCurrentUser } from "../../contexts/CurrentUserContexts";
 
 const Comment = (props) => {
-  const { profile_id, profile_img, owner, posted_at, content } = props;
+  const {
+    profile_id,
+    profile_img,
+    owner,
+    posted_at,
+    content,
+    id,
+    setTrack,
+    setComments,
+  } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setTrack((prevTrack) => ({
+        results: [
+          {
+            ...prevTrack.results[0],
+            comments_count: prevTrack.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -26,7 +55,7 @@ const Comment = (props) => {
           <span>
             <i class="fas fa-edit"></i>
           </span>
-          <span>
+          <span onClick={handleDelete}>
             <i class="fas fa-trash"></i>
           </span>
         </div>
