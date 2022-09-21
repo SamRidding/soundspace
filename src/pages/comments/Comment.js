f31a38433d5af995f08408bd4fdaa9379b931580
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-21 20:40:11 +0000

    Conditionally render edit/delete icons to comment owner

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import UserPic from "../../components/UserPic";
+import { useCurrentUser } from "../../contexts/CurrentUserContexts";
 
 const Comment = (props) => {
   const { profile_id, profile_img, owner, posted_at, content } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div>
@@ -17,6 +21,18 @@ const Comment = (props) => {
         </div>
       </div>
       <div>{posted_at}</div>
+      {is_owner ? (
+        <div>
+          <span>
+            <i class="fas fa-edit"></i>
+          </span>
+          <span>
+            <i class="fas fa-trash"></i>
+          </span>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
