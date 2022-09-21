834a1d3ef6280e5cc401de1395075ff82574d2e2
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-21 20:08:08 +0000

    Add props to comment

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import UserPic from "../../components/UserPic";
 
 const Comment = (props) => {
-    const { profile_id, profile_img, owner, posted_at, content } = props;
-    
-}
+  const { profile_id, profile_img, owner, posted_at, content } = props;
+
+  return (
+    <div>
+      <div>
+        <link to={`/profiles/${profile_id}`}>
+          <UserPic src={profile_img} />
+        </link>
+        <div>
+          <span>{owner}</span>
+          <p>{content}</p>
+        </div>
+      </div>
+      <div>{posted_at}</div>
+    </div>
+  );
+};
 
 export default Comment;
