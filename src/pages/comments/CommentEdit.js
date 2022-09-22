228168bd74f5738e937cd27988431ab4fa09dd7d
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-22 13:28:42 +0000

    Add basic form to comment edit

--- src/pages/comments/CommentEdit.js
+++ src/pages/comments/CommentEdit.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
 function CommentEdit(props) {
-    const { id, content, setShowEditForm, setComments } = props;
-    const [formContent, setFormContent] = useState(content);
+  const { id, content, setShowEditForm, setComments } = props;
+  const [formContent, setFormContent] = useState(content);
 
-    
+  return (
+    <div>
+      <form>
+        <input type="text"></input>
+        <button type="submit">Save</button>
+        <button type="button">Cancel</button>
+      </form>
+    </div>
+  );
 }
 
-export default CommentEdit
+export default CommentEdit;
