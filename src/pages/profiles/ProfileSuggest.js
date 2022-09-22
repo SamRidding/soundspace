31c1d31c63b0b72fd4a9da56c5ccfcdefa394211
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-22 20:56:57 +0000

    Add use state hook and destructure profile suggest

--- src/pages/profiles/ProfileSuggest.js
+++ src/pages/profiles/ProfileSuggest.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const ProfileSuggest = () => {
+  const [profileData, setProfileData] = useState({
+    profileSuggest: { results: [] },
+  });
+  const { profileSuggest } = profileData;
+
+
   return (
     <div>
-        <p>Users to follow</p>
+      <p>Users to follow</p>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileSuggest
+export default ProfileSuggest;
