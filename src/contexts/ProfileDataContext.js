be48e3b9ca110f2efc78fb967701700cbfa96ed4
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-23 15:50:22 +0000

    Add use state hook to destructure variables

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,5 +1,4 @@
-import React, { createContext, useContext } from 'react'
-
+import React, { createContext, useContext, useState } from "react";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
@@ -7,10 +6,13 @@ export const SetProfileDataContext = createContext();
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
 
-const ProfileDataContext = () => {
-  return (
-    <div>ProfileDataContext</div>
-  )
-}
+const ProfileDataContext = ({ children }) => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    profileSuggest: { results: [] },
+  });
+  
+  return <div>ProfileDataContext</div>;
+};
 
-export default ProfileDataContext
+export default ProfileDataContext;
