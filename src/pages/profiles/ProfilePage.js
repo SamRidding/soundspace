90206fd57653c50dbbe01d5503d0f27b61e6a14c
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-23 18:10:31 +0000

    Add template and styling for profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,8 +1,20 @@
 import React from "react";
-import styles from "../../styles/ProfilePage.module.css"
+import styles from "../../styles/ProfilePage.module.css";
 
 function ProfilePage() {
-  return <div></div>;
+  return (
+    <div className={styles.PPcontain}>
+      <div className={styles.PPflex}>
+        <div className={styles.PPleft}>
+          <div className={styles.PPuser}>
+            <p>User img/name</p>
+          </div>
+          <div className={styles.PPtracks}>Tracks</div>
+        </div>
+        <div className={styles.PPright}>User info, track likes.</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfilePage;
