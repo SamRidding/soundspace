f901c084f5609632bb45d962a2499559d2db64ef
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-23 15:44:50 +0000

    Add context objects and hooks for profile data

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { createContext, useContext } from 'react'
+
+
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
 
 const ProfileDataContext = () => {
   return (
