1f01bddd1d36135f3e56e720883b2e08260303ae
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-23 15:10:23 +0000

    Display user and follow button and add basic styling on profile.js

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import UserPic from "../../components/UserPic";
 import { useCurrentUser } from "../../contexts/CurrentUserContexts";
 import styles from "../../styles/Profile.module.css";
 
@@ -9,7 +11,25 @@ const Profile = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
-  return <div>Profile</div>;
+  return (
+    <div className={styles.Pcontain}>
+      <div className={styles.Puser}>
+        <Link to={`/profiles/${id}`}>
+          <UserPic src={image} height={imageSize} />
+        </Link>
+        <p>{owner}</p>
+      </div>
+      <div className={styles.Pfollow}>
+        {currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <button onClick={() => {}}>unfollow</button>
+          ) : (
+            <button onClick={() => {}}>follow</button>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
