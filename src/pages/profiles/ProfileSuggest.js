502525fddcac5ddca490404d467782e5f8cba408
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-23 17:28:36 +0000

    Import use profile data to profile suggest and replace code

--- src/pages/profiles/ProfileSuggest.js
+++ src/pages/profiles/ProfileSuggest.js
@@ -1,31 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { axiosReq } from "../../api/axiosDefaults";
-import { useCurrentUser } from "../../contexts/CurrentUserContexts";
+import React from "react";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 import Loading from "../../components/Loading";
 import Profile from "./Profile";
 
 const ProfileSuggest = () => {
-  const [profileData, setProfileData] = useState({
-    profileSuggest: { results: [] },
-  });
-  const { profileSuggest } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get("/profiles/");
-        setProfileData((prevState) => ({
-          ...prevState,
-          profileSuggest: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { profileSuggest } = useProfileData();
 
   return (
     <div>
