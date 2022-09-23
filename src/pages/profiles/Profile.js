4d77a30803af38fe17043c57602111cc89713407
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-23 14:32:33 +0000

    Import use current user and use for is owner variable

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useCurrentUser } from "../../contexts/CurrentUserContexts";
 import styles from "../../styles/Profile.module.css";
 
 const Profile = (props) => {
   const { profile, mobile, imageSize = 55 } = props;
   const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return <div>Profile</div>;
 };
