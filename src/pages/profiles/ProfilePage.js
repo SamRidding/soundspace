ad81326b87a342389410467475945597fdc69671
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-23 18:25:34 +0000

    Add use effect function to return profile data from API

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContexts";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 import styles from "../../styles/ProfilePage.module.css";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
-  const {id} = useParams();
+  const { id } = useParams();
+  const setProfileData = useSetProfileData();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [{ data: pageProfile }] = await Promise.all([
+          axiosReq.get(`/profiles/${id}/`),
+        ]);
+        setProfileData((prevState) => ({
+          ...prevState,
+          pageProfile: { results: [pageProfile] },
+        }));
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [id, setProfileData]);
 
   return (
     <div className={styles.PPcontain}>
