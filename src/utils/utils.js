d356d1da8d290935d0eb252fd107628a7c074a6c
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-24 10:50:00 +0000

    Add ternary to unfollow helper to deacrease followers/following count by -1

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,6 +27,14 @@ export const followHelper = (profile, clickedProfile, following_id) => {
     : profile;
 };
 
-export const unfollowHelper = () => {
-  return;
-}
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? { ...profile, following_count: profile.following_count - 1 }
+    : profile;
+};
