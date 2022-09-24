bd37d2f5be41b534588655f815a40a2535269c7d
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-24 21:24:43 +0000

    Add require imports and set variables for edit username

--- src/pages/profiles/EditUsername.js
+++ src/pages/profiles/EditUsername.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useHistory, useParams } from "react-router-dom";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContexts";
 
 const EditUsername = () => {
+  const [username, setUsername] = useState("");
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+  const { id } = useParams();
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
   return (
     <div className={styles.FormContain}>
       <Form onSubmit={handleSubmit} className={styles.Form}>
@@ -19,10 +31,12 @@ const EditUsername = () => {
           />
         </Form.Group>
 
-        <Button type="submit" className={styles.FormBtn}>Save</Button>
+        <Button type="submit" className={styles.FormBtn}>
+          Save
+        </Button>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default EditUsername
+export default EditUsername;
