eb3ff1462b8dfb51923d89b5a15f12e7f1d412e6
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-24 15:59:24 +0000

    Add edit button const to dropdown.js

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,14 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
-import styles from "../styles/Dropdown.module.css"
+import styles from "../styles/Dropdown.module.css";
 
+const EditBtn = React.forwardRef(({ onClick }, ref) => (
+  <i
+    class="fas fa-edit"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
