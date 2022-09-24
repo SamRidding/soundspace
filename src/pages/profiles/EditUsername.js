c229d7e26ecd75e433d0fed27d4c77cb3e90dddd
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-24 21:22:28 +0000

    Add form to edit username

--- src/pages/profiles/EditUsername.js
+++ src/pages/profiles/EditUsername.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 const EditUsername = () => {
   return (
-    <div>EditUsername</div>
+    <div className={styles.FormContain}>
+      <Form onSubmit={handleSubmit} className={styles.Form}>
+        <Form.Group>
+          <Form.Label className={styles.FormLabel}>Display name</Form.Label>
+          <Form.Control
+            className={styles.FormInput}
+            type="text"
+            placeholder="Enter new username"
+            onChange={handleChange}
+            name="username"
+            value={username}
+          />
+        </Form.Group>
+
+        <Button type="submit" className={styles.FormBtn}>Save</Button>
+      </Form>
+    </div>
   )
 }
 
