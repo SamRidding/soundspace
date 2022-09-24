207f105607ef7706d75420f10441ea990f731279
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-24 10:03:55 +0000

    Add handle follow func to profile data context provider

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -43,7 +43,7 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={setProfileData}>
+      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
