ce36319b4076fa66ce75cf89e3249e402d77fe94
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-25 10:07:21 +0000

    Add use state hook and desturcture user data

--- src/pages/profiles/EditPassword.js
+++ src/pages/profiles/EditPassword.js
@@ -1,36 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-import styles from "../../styles/FormPage.module.css"
+import styles from "../../styles/FormPage.module.css";
 import { useHistory, useParams } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContexts";
 
 const EditPassword = () => {
   const history = useHistory();
-  const {id} = useParams();
+  const { id } = useParams();
   const currentUser = useCurrentUser();
 
-  
+  const [userData, setUserData] = useState({
+    new_pw1: "",
+    new_pw2: "",
+  });
+  const { new_pwd1, new_pw2 } = userData;
 
   return (
     <div className={styles.FormContain}>
       <Form className={styles.Form}>
         <Form.Group>
           <Form.Label className={styles.FormLabel}>New Password</Form.Label>
-          <Form.Control
-            className={styles.FormInput}
-            type="text"
-          />
+          <Form.Control className={styles.FormInput} type="text" />
         </Form.Group>
 
         <Form.Group>
-          <Form.Label className={styles.FormLabel}>Confirm New Password</Form.Label>
-          <Form.Control
-            className={styles.FormInput}
-            type="text"
-          />
+          <Form.Label className={styles.FormLabel}>
+            Confirm New Password
+          </Form.Label>
+          <Form.Control className={styles.FormInput} type="text" />
         </Form.Group>
 
         <Button type="submit" className={styles.FormBtn}>
