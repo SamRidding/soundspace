77bf3b334cbf4f60f5d1ae73132d72e6937c174e
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-25 10:03:27 +0000

    Add form to editpassword.js

--- src/pages/profiles/EditPassword.js
+++ src/pages/profiles/EditPassword.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import styles from "../../styles/FormPage.module.css"
 
 const EditPassword = () => {
   return (
-    <div>EditPassword</div>
-  )
-}
+    <div className={styles.FormContain}>
+      <Form className={styles.Form}>
+        <Form.Group>
+          <Form.Label className={styles.FormLabel}>New Password</Form.Label>
+          <Form.Control
+            className={styles.FormInput}
+            type="text"
+          />
+        </Form.Group>
 
-export default EditPassword
+        <Form.Group>
+          <Form.Label className={styles.FormLabel}>Confirm New Password</Form.Label>
+          <Form.Control
+            className={styles.FormInput}
+            type="text"
+          />
+        </Form.Group>
+
+        <Button type="submit" className={styles.FormBtn}>
+          Save
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default EditPassword;
