b5347ae36c4ad69cba18ec1dbf5c259fa4fb69ca
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-26 20:05:34 +0000

    Comment out like tracks code

--- src/pages/tracks/LikedTracks.js
+++ src/pages/tracks/LikedTracks.js
@@ -1,11 +1,38 @@
-import React from "react";
+// import React, { useEffect, useState } from "react";
+// import { axiosReq } from "../../api/axiosDefaults";
+// import { useCurrentUser } from "../../contexts/CurrentUserContexts";
+// import Track from "./Track";
+// import Loading from "../../components/Loading";
 
-const LikedTracks = () => {
-  
-  return (
-    <div>
-    </div>
-  );
-};
+// const LikedTracks = () => {
+//   const currentUser = useCurrentUser();
+//   const userID = currentUser?.pk;
+//   const [userLikes, setUserLikes] = useState([]);
 
-export default LikedTracks;
+//   // const getUserLikes = async () => {
+//   //   const response = await axiosReq.get(
+//   //     `/tracks/?likes__owner__profile=${userID}`
+//   //   );
+//   //   console.log(response);
+//   // };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const data = await axiosReq.get(
+//           `/tracks/?likes__owner__profile=${userID}`
+//         );
+//         console.log(data.data);
+//         setUserLikes(data.data);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+
+//     fetchData();
+//   }, [userID]);
+
+//   return <div></div>;
+// };
+
+// export default LikedTracks;
