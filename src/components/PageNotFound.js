c88b08a2187e8bafd351cb4c34c42a6be24d8746
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-27 19:56:52 +0000

    Add page not found component and add styling

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles/PageNotFound.module.css";
+import { Link } from "react-router-dom";
+
+const PageNotFound = () => {
+  return (
+    <div className={styles.PNFcontain}>
+      <Link to={"/"} className={styles.PNFlink}>
+        <div className={styles.PNFmessage}>
+          <i class="fas fa-exclamation-circle fa-5x"></i>
+          <h1>Page not found</h1>
+          <p>Click here to return to the home page</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default PageNotFound;
